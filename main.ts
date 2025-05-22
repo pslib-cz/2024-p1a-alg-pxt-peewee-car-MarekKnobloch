d14145f04dcffa8781cefb2f68b0524e11f8f8a4
MarekKnobloch <marek.knobloch.024@pslib.cz>
2025-05-22 09:13:31 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,32 @@
 radio.setGroup(21)
 
-let x = 0
-let y = 0
+function controlCar(xTilt: number, yTilt: number) {
+    let speed = Math.map(yTilt, -1023, 1023, -200, 200)
+    speed = Math.constrain(speed, -200, 200)
+
+    let turn = Math.map(xTilt, -1023, 1023, -100, 100)
+    turn = Math.constrain(turn, -100, 100)
+
+    if (speed > 10) {
+        // Dopředu
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed - turn)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed + turn)
+    } else if (speed < -10) {
+        // Dozadu
+        PCAmotor.MotorRun(PCAmotor.Motors.M1, speed + turn)
+        PCAmotor.MotorRun(PCAmotor.Motors.M4, speed - turn)
+    } else {
+        // Stop
+        PCAmotor.MotorStopAll()
+    }
+}
+
+radio.onReceivedString(function (received: string) {
+    
+    let values = received.split(":")
+    if (values.length == 2) {
+        let x = parseInt(values[0])
+        let y = parseInt(values[1])
+        controlCar(x, y)
+    }
+})
